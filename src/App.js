097bd9fc1a32650a30be7a6aab4aabcace3ce981
G Gokul <ggokul1997@gmail.com>
2025-02-20 00:57:46 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Error from "./components/Error";
 
+// jdnjie
+
 function App() {
   return (
     <BrowserRouter>
